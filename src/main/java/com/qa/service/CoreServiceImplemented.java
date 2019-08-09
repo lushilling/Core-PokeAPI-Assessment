4541b09181cd72ab88cd7edc39e5512e11c714b6
lushilling <lshilling@hotmail.co.uk>
2019-08-09 11:34:22 +0100

    progress

--- src/main/java/com/qa/service/CoreServiceImplemented.java
+++ src/main/java/com/qa/service/CoreServiceImplemented.java
@@ -1,26 +1,39 @@
 package com.qa.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CoreServiceImplemented implements CoreService {
 
+	@Autowired
+	private RestTemplate rest;
+
 	public ResponseEntity<Object> addUser(Object user) {
-		// TODO Auto-generated method stub
+		String addUserRequest = rest.postForObject("http://locahost:8081/user/addUser", user, String.class);
 		return null;
 	}
 
 	public ResponseEntity<Object> userIdFindPokeByName(Long id, String name) {
-		// TODO Auto-generated method stub
+		String userRequest = rest.getForObject("http://localhost:8081/user/getUser/" + id, String.class);
+		String pokeRequest = rest.getForObject("http://localhost:8082/source/PokeByName/" + name, String.class);
+
+		// if user request returns 500 internal sever error
+		// print user does not exist
+
 		return null;
 	}
 
 	public ResponseEntity<Object> userIdFindPokeByNumber(Long id, String number) {
-		// TODO Auto-generated method stub
+		String userRequest = rest.getForObject("http://localhost:8081/user/getUser/" + id, String.class);
+		String pokeRequest = rest.getForObject("http://localhost:8082/source/PokeByNumber/" + number, String.class);
+
+		// if user request returns 500 internal sever error
+		// print user does not exist
+
 		return null;
 	}
 
-//	User Story: As a user, I want the system to inform me when I am trying to search for a Pokemon with a user that does not exist.
-	
 }
