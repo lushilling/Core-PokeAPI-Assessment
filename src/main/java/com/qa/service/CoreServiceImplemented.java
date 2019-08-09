ebe354933d89f7a91170ef7aa410bfdbe9cf79dd
lushilling <lshilling@hotmail.co.uk>
2019-08-09 11:55:38 +0100

    lunch

--- src/main/java/com/qa/service/CoreServiceImplemented.java
+++ src/main/java/com/qa/service/CoreServiceImplemented.java
@@ -1,7 +1,6 @@
 package com.qa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,29 +10,35 @@ public class CoreServiceImplemented implements CoreService {
 	@Autowired
 	private RestTemplate rest;
 
-	public ResponseEntity<Object> addUser(Object user) {
+	public String addUser(Object user) {
 		String addUserRequest = rest.postForObject("http://locahost:8081/user/addUser", user, String.class);
-		return null;
+		
+		// if request does NOT return status 201
+		// String result = "user not added"
+		
+		String result = "User added";
+		
+		return result;
 	}
 
-	public ResponseEntity<Object> userIdFindPokeByName(Long id, String name) {
+	public String userIdFindPokeByName(Long id, String name) {
 		String userRequest = rest.getForObject("http://localhost:8081/user/getUser/" + id, String.class);
 		String pokeRequest = rest.getForObject("http://localhost:8082/source/PokeByName/" + name, String.class);
 
 		// if user request returns 500 internal sever error
 		// print user does not exist
 
-		return null;
+		return pokeRequest;
 	}
 
-	public ResponseEntity<Object> userIdFindPokeByNumber(Long id, String number) {
+	public String userIdFindPokeByNumber(Long id, String number) {
 		String userRequest = rest.getForObject("http://localhost:8081/user/getUser/" + id, String.class);
 		String pokeRequest = rest.getForObject("http://localhost:8082/source/PokeByNumber/" + number, String.class);
 
 		// if user request returns 500 internal sever error
 		// print user does not exist
 
-		return null;
+		return pokeRequest;
 	}
 
 }
